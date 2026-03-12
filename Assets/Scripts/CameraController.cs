38abbd35fa8fa78f9c97c4aa9956557aee431666
beefchip <76847113+cheefbip@users.noreply.github.com>
2026-03-12 12:20:44 -0700

    Add Animated Water + Water Lock on Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraFollow : MonoBehaviour
     public float FollowSpeed = 2f;
     public float yOffset = 1f;
     public Transform target;
+    public Material waterMaterial;
+    public Transform floor;
 
     // Update is called once per frame
     Vector3 velocity = Vector3.zero;
@@ -15,5 +17,14 @@ public class CameraFollow : MonoBehaviour
     {
         Vector3 targetPos = new Vector3(target.position.x, target.position.y + yOffset, -8f);
         transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, 0.15f);
+
+        Vector2 offset = new Vector2(transform.position.x, transform.position.z) / -2560f;
+
+        waterMaterial.SetVector("_Offset", offset);
+
+        floor.localPosition = new Vector3(transform.position.x, floor.localPosition.y, transform.position.z);
+
+        
+        
     }
 }
